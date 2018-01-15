958b234df7f7afce7117bfc17d89d07955880419
Vladimir Koljasinskij <smosker@yandex-team.ru>
2018-01-15 13:17:20 +0300

    change setup

--- setup.py
+++ setup.py
@@ -39,7 +39,9 @@ setup(
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.5',
                  ],
+    keywords='django postgresql migrations',
     packages=find_packages(),
+    python_requires='>=2.7,!=3.0.*,!=3.1.*!=3.2.*,!=3.3.*,!=3.4.*',
     install_requires=[
         'Django>=1.3',
         'psycopg2>=2.7.3.2',
