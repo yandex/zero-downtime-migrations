772414327c90a9f934db38b1749a7f7dedb87fa1
Vladimir Koljasinskij <smosker@yandex-team.ru>
2019-07-03 14:35:52 +0300

    fix tests

--- tests/test_add_unique.py
+++ tests/test_add_unique.py
@@ -28,18 +28,22 @@ def test_add_unique_correct_queries():
     field.set_attributes_from_name("name")
     if DJANGO_VERISON >= StrictVersion('2.1'):
         index_pattern = r'CREATE UNIQUE INDEX CONCURRENTLY "test_app_testmodel_name_\w+(_uniq)?" ON "test_app_testmodel" \("name"\)'
+        check_index_pattern = r"SELECT 1 FROM pg_class, pg_index WHERE pg_index.indisvalid = false AND pg_index.indexrelid = pg_class.oid and pg_class.relname = 'test_app_testmodel_name_\w+(_uniq)?'"
         constraint_pattern = r'ALTER TABLE test_app_testmodel ADD CONSTRAINT test_app_testmodel_name_\w+(_uniq)? UNIQUE USING INDEX test_app_testmodel_name_\w+(_uniq)?'
-        expected_queries = 2
+        expected_queries = 3
     else:
         index_pattern = r'ALTER TABLE "test_app_testmodel" ADD CONSTRAINT "?test_app_testmodel_name_\w+(_uniq)?"? UNIQUE \("name"\)'
+        check_index_pattern = None
         constraint_pattern = None
         expected_queries = 1
     with CaptureQueriesContext(connection) as ctx, schema_editor(connection=connection) as editor:
         editor.alter_field(TestModel, old_field, field)
         assert len(ctx.captured_queries) == expected_queries
         assert re.search(index_pattern, ctx.captured_queries[0]['sql']) is not None
+        if check_index_pattern:
+            assert re.search(index_pattern, ctx.captured_queries[1]['sql']) is not None
         if constraint_pattern:
-            assert re.search(constraint_pattern, ctx.captured_queries[1]['sql']) is not None
+            assert re.search(constraint_pattern, ctx.captured_queries[2]['sql']) is not None
 
 
 @pytest.mark.django_db(transaction=True)
